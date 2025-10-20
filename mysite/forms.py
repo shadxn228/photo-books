181ebd0f47413adee73feaa79001db8e0c1b0a8c
John Baby <i.445@mail.ru>
2025-10-20 17:41:38 +0300

    created and configured files for upload image function

--- mysite/forms.py
+++ mysite/forms.py
@@ -1,7 +1,25 @@
 from django import forms
-from .models import Projects
+from .models import Projects, Photos
 
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Projects
         fields = ["title", "template"]
+
+
+class PhotoForm(forms.ModelForm):
+    class Meta:
+        model = Photo
+        fields = ['title', 'image', 'description']
+        widgets = {
+            'url': forms.HiddenInput,
+        }
+
+        def clean_url(self):
+            url = self.cleaned_data['url']
+            valid_extensions = ['jpg', 'jpeg', 'png']
+            extension = url.rsplit('.', 1)[1].lower()
+            if extension not in valid_extensions:
+                raise forms.ValidationError('The given URL does not ' \
+                    'match valid image extensions.')
+            return url
